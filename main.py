c0e326eeda637b03768846894d51a312d71c4e35
Ryder Cook <48497255+RC00K@users.noreply.github.com>
2022-09-17 21:41:16 -0500

    Pull 5 Stock Prices

--- main.py
+++ main.py
@@ -2,26 +2,44 @@
 # Ryder Cook
 # Mini Project 1
 
+import os
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = yf.download("AAPL", start="2022-09-04", end="2022-09-14")
+# Five stocks
+ticker_names = ["AAPL", "GOOGL", "BTC-USD", "TSLA", "META"]
+# Collect data from last ten days
+data = yf.download(tickers=ticker_names, start="2022-09-07", end="2022-09-17")
 
-aaplPrices = []
+# Save plot as png in new folder
+def new_direct(direct):
+    # Make a folder
+    if not os.path.exists(direct):
+        os.mkdir(direct)
 
-for price in data['Adj Close']:
-    aaplPrices.append(price)
 
-print(aaplPrices)
+# Get the closing prices from five stocks
+def closing_prices(ticker):
+    return [price for price in data['Adj Close'][ticker]]
 
-# Create a numpy array
-aaplarray = np.array(aaplPrices)
-# Create matplotlib graph
-plt.plot(aaplarray)
 
-# Save plot as png
-plt.savefig('charts/aapl.png')
+# Draw the charts for five stocks and save to new folder
+def draw_charts(tickers):
+    chart_dir = "charts"
+    new_direct(chart_dir)
+    for ticker in tickers:
+        ticker_price = np.array(closing_prices(ticker))
+        x = np.linspace(0.1, 2 * np.pi, 41)
+        y = np.exp(np.sin(x))
+        plt.stem(ticker_price)
+        # Fancy chart output
+        plt.title(f"{ticker} Stock Price the Last 10 Days")
+        plt.xlabel("Day")
+        plt.ylabel("Price")
+        # Save plot as png in new folder
+        plt.savefig(os.path.join(chart_dir, f"{ticker}.png"))
+        plt.close()
 
-# Show graph
-plt.show()
+
+draw_charts(ticker_names)
